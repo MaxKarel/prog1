146251ca9ef454977024677dd2897954db49b48b
Max Karel <maxdkarel@gmail.com>
2017-09-26 15:40:46 +0200

    initial commit
      * Lesson #0

---
+++ 0/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int a, b, add;
+
+	cin >> a >> b >> add;
+
+	//RETURN
+	cout << "nove ceny: " << a + add << ", " << b + add << endl;
+	
+	return 0;
+}
